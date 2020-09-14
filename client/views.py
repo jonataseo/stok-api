ba61d2952ebf17524c756eee22432a5492572d70
jonatas <jonatas.eoliveira@gmail.com>
2020-09-13 23:12:34 -0400

    Adding ViewSet for client and config for pagination

--- client/views.py
+++ client/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Client
+from .serializers import ClientSerializer
+
+
+class DefaultMinix(object):
+    """Pagination config"""
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+class ClientViewSet(viewsets.ModelViewSet):
+    """Endpoints for list and create clients"""
+
+    queryset = Client.objects.order_by('name')
+    serializer_class = ClientSerializer
